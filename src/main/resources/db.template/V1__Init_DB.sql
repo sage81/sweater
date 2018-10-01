f55edb795cf0277be54897397cbb311e0cdaa2af
sage81 <yvakarchuk@gmail.com>
2018-10-01 03:05:21 +0300

    flyWay

---
+++ src/main/resources/db.template/V1__Init_DB.sql
@@ -0,0 +1,33 @@
+create table hibernate_sequence (next_val bigint);
+
+create table message (
+    id integer not null,
+    filename varchar(255),
+    tag varchar(255),
+    text varchar(2048),
+    user_id long,
+    primary key (id)
+);
+
+create table user_role (
+    user_id bigint not null,
+    roles varchar(255)
+);
+
+create table usr (
+    id long not null,
+    activation_code varchar(255),
+    active bit not null,
+    email varchar(255),
+    password varchar(255),
+    username varchar(255),
+    primary key (id)
+);
+
+alter table message
+    add constraint message_user_fk
+    foreign key (user_id) references usr (id);
+
+alter table user_role
+    add constraint user_role_user_fk
+    foreign key (user_id) references usr (id);
