458f15e96bbdea5fd38c569a1a79f66c22b4397f
sage81 <yvakarchuk@gmail.com>
2018-10-19 00:09:31 +0300

    flyWay

--- src/main/java/ua/yava/sweater/repos/UserRepo.java
+++ src/main/java/ua/yava/sweater/repos/UserRepo.java
@@ -4,6 +4,7 @@ import ua.yava.sweater.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User, Long> {
+
     User findByUsername(String username);
 
     User findByActivationCode(String code);
